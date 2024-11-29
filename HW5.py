e32d384e5bc30efd735371a66eab019cf3b1c15e
chanwooyoon1 <chanwooyoon@arizona.edu>
2024-11-29 13:20:04 -0700

    Update HW5.py

--- HW5.py
+++ HW5.py
@@ -1,5 +1,4 @@
-# this code is created by Chanwoo Yoon and Juwon Lee
-# only one pair of training and validation sets are considerd in this program
+# This program is created by Chanwoo Yoon and Juwon Lee
 
 import torch
 from torch import nn
@@ -46,24 +45,71 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
-        )
+        self.linear_relu_stack = None
+        self.layers = ""
+
+    def set_layers(self, layers, activation):
+        self.layers = layers
+        if activation == "ReLU":
+            activation_fn = nn.ReLU()
+        elif activation == "Tanh":
+            activation_fn = nn.Tanh()
+        else:
+            activation_fn = nn.SiLU()
+        if layers == "small":
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(28*28, 10),
+                activation_fn,
+                nn.Linear(10, 10)
+            )
+        if layers == "medium":
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(28*28, 512),
+                activation_fn,
+                nn.Linear(512, 512),
+                activation_fn,
+                nn.Linear(512, 512),
+                activation_fn,
+                nn.Linear(512, 512),
+                activation_fn,
+                nn.Linear(512, 512),
+                activation_fn,
+                nn.Linear(512, 10)
+            )
+        if layers == "big":
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(28*28, 512),
+                activation_fn,
+                nn.Linear(512, 512),
+                activation_fn,
+                nn.Linear(512, 512),
+                activation_fn,
+                nn.Linear(512, 512),
+                activation_fn,
+                nn.Linear(512, 512),
+                activation_fn,
+                nn.Linear(512, 512),
+                activation_fn,
+                nn.Linear(512, 512),
+                activation_fn,
+                nn.Linear(512, 10)
+            )
+
+
 
     def forward(self, x):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
 
+    def get_layers(self):
+        return self.layers
+
 class NNProcessor():
     def __init__(self, id):
         self.model = NeuralNetwork().to(device)
         self.loss_fn = nn.CrossEntropyLoss()
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-3) # lr: learning rate
+        self.optimizer = None
         self.train_set = None
         self.val_set = None
         self.train_loss = 0
@@ -129,10 +175,134 @@ class NNProcessor():
     def get_val_accuracy(self):
         return self.val_accuracy
 
+    def set_layers(self, layers, activation):
+        self.model.set_layers(layers, activation)
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-1) # lr: learning rate
+
     def get_parameters(self):
         return self.model.state_dict()
 
-def run_Epoch(training_data, epochs, k):
+    def get_layers(self):
+        return self.model.get_layers()
+
+def run_epoch_different_architecture(training_data, epochs, k):
+    kf = KFold(n_splits=k, shuffle=True, random_state=42)
+    NNProcessors = [[], [], []]
+    for i in range(len(NNProcessors)):
+        for j in range(k):
+            NNP = NNProcessor(j)
+            NNProcessors[i].append(NNP)
+
+    for fold, (train_idx, val_idx) in enumerate(kf.split(range(len(training_data)))):
+        train_subset = Subset(training_data, train_idx)
+        val_subset = Subset(training_data, val_idx)
+        # split our data into actual training and validation sets
+        train_dataloader = DataLoader(train_subset, batch_size=64, shuffle=True)
+        val_dataloader = DataLoader(val_subset, batch_size=64, shuffle=False)
+        # feed two splitted data to corresponding ProcessFold() object
+        NNProcessors[0][fold].set_data(train_dataloader, val_dataloader)
+        NNProcessors[1][fold].set_data(train_dataloader, val_dataloader)
+        NNProcessors[2][fold].set_data(train_dataloader, val_dataloader)
+    # a list to store NNPs with three different structure
+    models = [[], [], []]
+    layers = ["medium", "medium", "medium"]
+    activation = ["ReLU", "Tanh", "Swish"]
+
+    for i in range(len(models)):
+        for j in range(3):
+            NNP = NNProcessors[i][j]
+            models[i].append(NNP)
+
+    for i in range(len(models)):
+        for j in range(len(models[i])):
+            models[i][j].set_layers(layers[i], activation[i])
+
+
+    train_accuracies = [[], [], []]
+    val_accuracies = [[], [], []]
+    smallest_validation_losses = [None, None, None]
+    best_model_parameters = [None, None, None]
+    result = []
+    for e in range(epochs):
+        # this list stores three pairs of training loss and validation loss
+        # the three pairs correspond to the thee models with different architectures
+        sub_result = []
+        for j in range(len(models)): # 3
+            training_losses = []
+            validation_losses = []
+            for i in range(len(models[j])): # 3
+                models[j][i].train()
+                models[j][i].validate()
+                if smallest_validation_losses[j] == None or smallest_validation_losses[j] > models[j][i].get_val_loss():
+                    smallest_validation_losses[j] = models[j][i].get_val_loss()
+                    best_model_parameters[j] = models[j][i].get_parameters()
+                training_losses.append(models[j][i].get_train_loss())
+                validation_losses.append(models[j][i].get_val_loss())
+                train_accuracies[j].append(models[j][i].get_train_accuracy())
+                val_accuracies[j].append(models[j][i].get_val_accuracy())
+            sub_result.append((training_losses, validation_losses))
+        result.append(sub_result)
+        print("Epoch " + str(e+1) + " done")
+    print("All " + str(epochs) + " epochs are done")
+    best_val_accuracies = [[], [], []]
+    for i in range(len(val_accuracies)):
+        sorted_list = sorted(val_accuracies[i])[::-1]
+        best_val_accuracies[i].append(sorted_list[0])
+        best_val_accuracies[i].append(sorted_list[1])
+        best_val_accuracies[i].append(sorted_list[2])
+    for i in range(3):
+        print("\n" + layers[i] + " model with " + activation[i] + " non-linear function: ")
+        print("Average training accuracy-> " + str(round(sum(train_accuracies[i])/len(train_accuracies[i]), 2)) + "%")
+        print("Standard error of the mean of the training accuracy-> " + str(round(np.std(train_accuracies[i]), 2)) + "\n")
+        print("Average validation accuracy-> " + str(round(sum(val_accuracies[i])/len(val_accuracies[i]), 2)) + "%")
+        print("Standard error of the mean of the validation accuracy-> " + str(round(np.std(val_accuracies[i]), 2)) + "\n")
+        print("Average validation accuracy for the best model for each validation set-> " + str(round(sum(best_val_accuracies[i])/len(best_val_accuracies[i]), 2)) + "%")
+        print("Standard error of the mean of the validation accuracy of the best models-> " + str(round(np.std(best_val_accuracies[i]), 2)) + "\n")
+    return result, best_model_parameters
+
+
+def plot_different_architecture(result):
+    epochs_range = range(len(result))
+    average_train_losses = [[], [], []]
+    average_val_losses = [[], [], []]
+    std_train = [[], [], []]
+    std_val = [[], [], []]
+    for epoch in result:
+        for i in range(len(epoch)):
+            average_train_losses[i].append(sum(epoch[i][0])/len(epoch[i][0]))
+            average_val_losses[i].append(sum(epoch[i][1])/len(epoch[i][1]))
+            std_train[i].append(np.std(epoch[i][0]))
+            std_val[i].append(np.std(epoch[i][1]))
+    colors_train = ['r', 'g', 'b']
+    colors_val = ['pink', 'lightgreen', 'skyblue']
+
+    for i in range(len(average_train_losses)):
+        for j in range(len(average_train_losses[i])):
+            plt.plot(j, average_train_losses[i][j], 'o', color = colors_train[i])
+            plt.plot(j, average_val_losses[i][j], 'o', color = colors_val[i])
+    for i in range(len(std_train)):
+        plt.fill_between(
+            epochs_range,
+            [average_train_losses[i][j] - std_train[i][j] for j in range(len(std_train[i]))],
+            [average_train_losses[i][j] + std_train[i][j] for j in range(len(std_train[i]))],
+            color=colors_train[i],
+            alpha=0.2
+        )
+        plt.fill_between(
+            epochs_range,
+            [average_val_losses[i][j] - std_val[i][j] for j in range(len(std_val[i]))],
+            [average_val_losses[i][j] + std_val[i][j] for j in range(len(std_val[i]))],
+            color=colors_val[i],
+            alpha=0.2
+        )
+    plt.title('Red: ReLU, Green: Tanh, Blue: Swish')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.show()
+
+
+
+def run_Epoch(training_data, epochs, k, layers):
     kf = KFold(n_splits=k, shuffle=True, random_state=42)
     NNProcessors = []
     for i in range(k):
@@ -148,16 +318,19 @@ def run_Epoch(training_data, epochs, k):
         NNProcessors[fold].set_data(train_dataloader, val_dataloader)
     result = []
     smaller_NNPs = []
-    # we are processing three NNPs instead of k
+    # we are processing one NNPs instead of k
     smaller_NNPs.append(NNProcessors[0])
-    smaller_NNPs.append(NNProcessors[1])
-    smaller_NNPs.append(NNProcessors[2])
+    #smaller_NNPs.append(NNProcessors[1])
+    #smaller_NNPs.append(NNProcessors[2])
+    for NNP in smaller_NNPs:
+        NNP.set_layers(layers)
 
     train_accuracies = []
     val_accuracies0 = []
     val_accuracies1 = []
     val_accuracies2 = []
-    val_accuracies = [val_accuracies0, val_accuracies1, val_accuracies2]
+    #val_accuracies = [val_accuracies0, val_accuracies1, val_accuracies2]
+    val_accuracies = [val_accuracies0]
     smallest_validation_loss = None
     best_model_parameter = None
     for e in range(epochs):
@@ -232,8 +405,9 @@ def plot(alist):
     plt.ylabel('Loss')
     plt.show()
 
-def final_check(best_model_parameter):
+def final_check(best_model_parameter, layers):
     model = NeuralNetwork().to(device)
+    model.set_layers(layers)
     model.load_state_dict(best_model_parameter)
     model.eval()
     loss_fn = nn.CrossEntropyLoss()
@@ -261,14 +435,70 @@ def final_check(best_model_parameter):
     test_correct /= size
     print(f"Testing accuracy on the best model-> {(100*test_correct):>0.1f}%")
 
+def final_check_different_architecture(best_model_parameters):
+    layers = ["small", "medium", "big"]
+    models = []
+    model1 = NeuralNetwork().to(device)
+    model2 = NeuralNetwork().to(device)
+    model3 = NeuralNetwork().to(device)
+    model1.set_layers("medium", "ReLU")
+    model2.set_layers("medium", "Tanh")
+    model3.set_layers("medium", "Swish")
+    model1.load_state_dict(best_model_parameters[0])
+    model2.load_state_dict(best_model_parameters[1])
+    model3.load_state_dict(best_model_parameters[2])
+    model1.eval()
+    model2.eval()
+    model3.eval()
+    models.append(model1)
+    models.append(model2)
+    models.append(model3)
+
+    loss_fn = nn.CrossEntropyLoss()
+    training_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    idx = 0
+    for model in models:
+        print("\n" + layers[idx] + ": ")
+        idx += 1
+        train_loss, train_correct = 0, 0
+        test_loss, test_correct = 0, 0
+        size = len(training_dataloader.dataset)
+        with torch.no_grad():
+            for X, y in training_dataloader:
+                X, y = X.to(device), y.to(device)
+                pred = model(X)
+                train_loss += loss_fn(pred, y).item()
+                train_correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        train_correct /= size
+        print(f"Training accuracy on the best model-> {(100*train_correct):>0.1f}%")
+
+        size = len(test_dataloader.dataset)
+        with torch.no_grad():
+            for X, y in test_dataloader:
+                X, y = X.to(device), y.to(device)
+                pred = model(X)
+                test_loss += loss_fn(pred, y).item()
+                test_correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        test_correct /= size
+        print(f"Testing accuracy on the best model-> {(100*test_correct):>0.1f}%")
+
 def main():
-    epochs = 150
+    layers = "big"
+    epochs = 30
     k = 10
-    result, best_model_parameter = run_Epoch(training_data, epochs, k)
-    final_check(best_model_parameter)
+    result, best_model_parameter = run_Epoch(training_data, epochs, k, layers)
+    final_check(best_model_parameter, layers)
     plot(result)
 
-main()
+def main1():
+    epochs = 30
+    k = 10
+    result, best_model_parameters = run_epoch_different_architecture(training_data, epochs, k)
+    final_check_different_architecture(best_model_parameters)
+    plot_different_architecture(result)
+
+main1()
 
 
 
